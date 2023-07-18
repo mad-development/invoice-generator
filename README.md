f0509a975dae19d22e33c2e9543216c2aae61312
Madson Junior <madson.junior@maddev.net>
2023-07-18 17:10:00 -0300

    feat: minor adjustments on invoice layout

--- README.md
+++ README.md
@@ -9,7 +9,6 @@ Basic execution:
 go run main.go -day=18 \
                -month=7 \
                -year=2023 \
-               -value=200.50 \
                -client="John Doe" \
                -id="INV1234" \
                -services="Service 1:100.0,Service 2:75.0,Service 3:25.5" \
--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func (i *Invoice) GeneratePDF() {
 	pdf.CellFormat(40, 10, fmt.Sprintf("%s", i.originName), "0", 0, "", false, 0, "")
 	pdf.Ln(10)
 	pdf.SetFont("Arial", "", 12)
-	pdf.CellFormat(40, 10, fmt.Sprintf("Address: %s", i.originAddress), "0", 0, "", false, 0, "")
+	pdf.CellFormat(40, 10, i.originAddress, "0", 0, "", false, 0, "")
 	pdf.Ln(20)
 
 	// Write the invoice details
@@ -55,12 +55,12 @@ func (i *Invoice) GeneratePDF() {
 	pdf.SetFont("Arial", "", 12)
 	for service, price := range i.services {
 		pdf.CellFormat(90, 10, service, "1", 0, "", false, 0, "")
-		pdf.CellFormat(100, 10, fmt.Sprintf("%.2f %s", price, i.currency), "1", 0, "", false, 0, "")
+		pdf.CellFormat(100, 10, fmt.Sprintf("%s %.2f", i.currency, price), "1", 0, "", false, 0, "")
 		pdf.Ln(10)
 	}
 	pdf.Ln(10)
 
-	pdf.CellFormat(40, 10, fmt.Sprintf("Total Value: %.2f %s", i.value, i.currency), "0", 0, "", false, 0, "")
+	pdf.CellFormat(40, 10, fmt.Sprintf("Total Value: %s %.2f", i.currency, i.value), "0", 0, "", false, 0, "")
 
 	err := pdf.OutputFileAndClose("invoice.pdf")
 	if err != nil {
@@ -74,7 +74,6 @@ func main() {
 	day := flag.Int("day", now.Day(), "Invoice day")
 	month := flag.Int("month", int(now.Month()), "Invoice month")
 	year := flag.Int("year", now.Year(), "Invoice year")
-	value := flag.Float64("value", 0.0, "Invoice value")
 	client := flag.String("client", "", "Client name")
 	invoiceID := flag.String("id", "", "Invoice ID")
 	services := flag.String("services", "", "Comma separated list of services in the format 'name:price'")
@@ -83,11 +82,12 @@ func main() {
 
 	flag.Parse()
 
-	if *day == 0 || *month == 0 || *year == 0 || *value == 0.0 || *client == "" || *invoiceID == "" || *services == "" || *originName == "" || *originAddress == "" {
+	if *day == 0 || *month == 0 || *year == 0 || *client == "" || *invoiceID == "" || *services == "" || *originName == "" || *originAddress == "" {
 		fmt.Println("Please provide all required parameters.")
 		return
 	}
 
+	value := 0.0
 	serviceList := make(map[string]float64)
 	servicePairs := strings.Split(*services, ",")
 	for _, pair := range servicePairs {
@@ -104,14 +104,15 @@ func main() {
 			return
 		}
 		serviceList[serviceName] = servicePrice
+		value += servicePrice
 	}
 
 	date := time.Date(*year, time.Month(*month), *day, 0, 0, 0, 0, time.UTC)
 
 	invoice := &Invoice{
 		date:          date,
-		value:         *value,
-		currency:      "USD",
+		value:         value,
+		currency:      "$",
 		client:        *client,
 		invoiceID:     *invoiceID,
 		services:      serviceList,
